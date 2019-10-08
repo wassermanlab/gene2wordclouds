5c4b0cc944a7b71d81f57363cdb3503fce9d2e6b
Oriol Fornés <oriol@cmmt.ubc.ca>
2019-10-07 18:33:22 -0700

    everything

--- tf-idfs/get_tf_idfs.py
+++ tf-idfs/get_tf_idfs.py
@@ -2,8 +2,9 @@
 
 import argparse
 from copy import deepcopy
-from enchant.tokenize import get_tokenizer
 from fuzzywuzzy import fuzz
+import gzip
+from itertools import product
 from numpy import log10 as log, nan
 import os
 import pandas
@@ -12,6 +13,7 @@ import pyreadr
 import re
 import ssl
 import subprocess
+from unidecode import unidecode
 from urllib import parse, request
 
 #-------------#
@@ -28,6 +30,7 @@ def parse_args():
     parser.add_argument("-i", default="./", help="idfs directory (i.e. output directory from get_idfs.py)", metavar="DIR")
     parser.add_argument("-p", default="./", help="pmids directory (i.e. output directory from get_pmids.py)", metavar="DIR")
     parser.add_argument("-o", default="./", help="output directory (default = ./)", metavar="DIR")
+    parser.add_argument("-w", default=50, type=int, help="max. number of words (default = 50)", metavar="INT")
 
     return(parser.parse_args())
 
@@ -37,27 +40,41 @@ def main():
     args = parse_args()
 
     # Make files
-    get_tf_idfs(os.path.abspath(args.i), os.path.abspath(args.p), os.path.abspath(args.o))
+    get_tf_idfs(os.path.abspath(args.i), os.path.abspath(args.p), os.path.abspath(args.o), args.w)
 
-def get_tf_idfs(idfs_dir, pmids_dir, output_dir="./"):
+def get_tf_idfs(idfs_dir, pmids_dir, output_dir="./", max_words=50):
 
     # Initialize
-    taxons = ["fungi", "insects", "nematodes", "plants", "vertebrates"]
     regexp = re.compile("^\w.+\w$")
+    taxons = ["fungi", "insects", "nematodes", "plants", "vertebrates"]
+    thresh = 89.0
+    aa = ['ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN', 'GLU', 'GLY', 'HIS', 'ILE',
+        'LEU', 'LYS', 'MET', 'PHE', 'PRO', 'SER', 'THR', 'TRP', 'TYR', 'VAL']
+
+    # Get all aminoacid pairs
+    aminoacid_pairs = set(["".join(c) for c in product([a.lower() for a in aa], repeat=2)])
 
     # Globals
     global cwd
     cwd = os.getcwd()
 
     tfs = {
+
+        "P01100": "FOS",
+        "P05412": "JUN",
+        "O75364": "PITX3",
         "P04637": "TP53",
         "P19838": "NFKB1",
         "P49711": "CTCF",
+        "P41235": "HNF4A",
     }
 
     # Create output directory
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
+
+    # Move to output directory
+    os.chdir(output_dir)
 
     # Load pickle file
     pickle_file = os.path.join(pmids_dir, ".uniacc2entrezid.pickle")
@@ -68,6 +85,9 @@ def get_tf_idfs(idfs_dir, pmids_dir, output_dir="./"):
     pickle_file = os.path.join(pmids_dir, ".entrezid2pmid.pickle")
     with open(pickle_file, "rb") as f:
         entrezid2pmid = pickle.load(f)
+
+    # Get entrezid to gene name mappings
+    entrezid2genename = _get_uniacc_to_genename_mappings(uniacc2entrezid)
 
     # For each taxon...
     for taxon in taxons:
@@ -94,9 +114,6 @@ def get_tf_idfs(idfs_dir, pmids_dir, output_dir="./"):
         with open(os.path.join(idfs_dir, "idfs.%s.pickle" % taxon), "rb") as f:
             idfs = pickle.load(f)
 
-        # Get uniacc to gene name mappings
-        uniacc2genename = _get_uniacc_to_genename_mappings(uniaccs)
-
         for uniacc in sorted(uniaccs):
 
             if uniacc not in tfs:
@@ -110,6 +127,7 @@ def get_tf_idfs(idfs_dir, pmids_dir, output_dir="./"):
                 # Initialize
                 df = None
                 tfidfs = {}
+                word_count = 0
 
                 # Skip if uniacc not mapped to an entrezid
                 if uniacc not in uniacc2entrezid:
@@ -161,97 +179,144 @@ def get_tf_idfs(idfs_dir, pmids_dir, output_dir="./"):
                 # Get TF-IDFs
                 df["tfidf"] = df["idf"] * df["tf"]
 
-                # Filter weird words + TF containing words
-                tknzr = get_tokenizer("en")
-                gene_name = uniacc2genename[uniacc]
-                thresh = 66.0
+                # Sort data frame by TF-IDFs
+                df = df.sort_values(by="tfidf", ascending=False).reset_index()
+
+                # Filter weird words
                 for idx, row in df.iterrows():
-                    if not regexp.match(row["Var1"]):
-                        tokens = [w for w in tknzr(row["Var1"])]
-                        if len(tokens) == 0:
-                            df["tfidf"][idx] = -1
-                        elif tokens[0][1] > 0:
-                            df["tfidf"][idx] = -1
-                        elif (len(tokens[-1][0]) + tokens[-1][1]) < len(row["Var1"]):
-                            df["tfidf"][idx] = -1
-                    # Fuzzy string matching
-                    if fuzz.partial_ratio(gene_name.lower(), row["Var1"]) > 50:
-                        print(gene_name.lower(), row["Var1"], fuzz.partial_ratio(gene_name.lower(), row["Var1"]))
+
+                    if word_count >= max_words:
                         df["tfidf"][idx] = -1
-                    # for token in tokens:
-                    #     if fuzz.partial_ratio(gene_name.lower(), token) > thresh or fuzz.partial_ratio(token, gene_name.lower()) > thresh:
-                            
-                    #         df["tfidf"][idx] = -1
-                    #         break
+
+                    if df["tfidf"][idx] != -1:
+
+                        # Filter weird words
+                        if unidecode(row["Var1"]) in aminoacid_pairs:
+                            df["tfidf"][idx] = -1
+                            continue
+
+                        if not regexp.match(unidecode(row["Var1"])):
+                            df["tfidf"][idx] = -1
+                            continue
+
+                        # Filter redundant words
+                        for nextidx, nextrow in df.iterrows():
+
+                            # i.e. time saver
+                            if nextidx > 250 or nextidx <= idx or df["tfidf"][nextidx] == -1:
+                                continue
+
+                            ratio = fuzz.ratio(unidecode(row["Var1"]), unidecode(nextrow["Var1"]))
+                            partial_ratio = fuzz.partial_ratio(unidecode(row["Var1"]), unidecode(nextrow["Var1"]))
+
+                            if ratio >= thresh or partial_ratio >= thresh:
+                                df["tfidf"][nextidx] = -1
+
+                        # Tokenize
+                        tkns = re.findall(r'\w+', unidecode(row["Var1"]))
+
+                        # Filter TF-containing words
+                        for genename in entrezid2genename[uniacc2entrezid[uniacc]]:
+
+                            global_ratio = fuzz.ratio(genename, unidecode(row["Var1"]))
+
+                            # Initialize
+                            gene_tkns = re.findall(r'\w+', genename.lower())
+
+                            for tkn in tkns:
+
+                                if len(tkn) == 1:
+                                    continue
+
+                                if df["tfidf"][idx] == -1:
+                                    break
+
+                                for gene_tkn in gene_tkns:
+
+                                    if len(gene_tkn) == 1:
+                                        continue
+
+                                    ratio = fuzz.ratio(gene_tkn, tkn)
+                                    partial_ratio = fuzz.partial_ratio(gene_tkn, tkn)
+
+                                    if (ratio + partial_ratio) / 2.0 >= thresh:
+                                        df["tfidf"][idx] = -1
+                                        break
+
+                    if df["tfidf"][idx] != -1:
+                        word_count += 1
 
                 # Write RDS file
                 pyreadr.write_rds(out_file, df)
 
             # Make word cloud
             # _make_word_cloud(uniacc)
-            _make_word_cloud(tfs[uniacc])
+            _make_word_cloud(tfs[uniacc], max_words)
 
         # Return to original directory
         os.chdir(cwd)
 
-def _get_uniacc_to_genename_mappings(uniaccs):
+def _get_uniacc_to_genename_mappings(uniacc2entrezid):
 
     # Initialize
-    uniacc2genename = {}
-    gcontext = ssl.SSLContext()
-    url = "https://www.uniprot.org/uploadlists/"
+    entrezids = set()
+    entrezid2genename = {}
+    url = "ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/"
+    file_name = "gene_info.gz"
 
-    # # Skip if pickle file already exists
-    # pickle_file = ".uniacc2genename.pickle"
-    # if not os.path.exists(pickle_file):
+    # Skip if pickle file already exists
+    pickle_file = ".entrezid2genename.pickle"
+    if not os.path.exists(pickle_file):
 
-    # Set query
-    params = {
-        "from": "ACC+ID",
-        "to": "GENENAME",
-        "format": "tab",
-        "query": " ".join(list(uniaccs))
-    }
+        # For each uniacc, entrezid pair...
+        for uniacc, entrezid in uniacc2entrezid.items():
+            entrezids.add(entrezid)
 
-    # Encode parameters
-    params = parse.urlencode(query=params).encode("utf-8")
+        # Download gene to pubmed mappings
+        request.urlretrieve(os.path.join(url, file_name), file_name)
 
-    # Make request
-    req = request.Request(url, params)
+        with gzip.open(file_name, "r") as f:
 
-    # Get response
-    with request.urlopen(req, context=gcontext) as f:
-        response = f.read().decode("utf-8")
+            # For each line
+            for line in f:
 
-    # For each line...
-    for line in response.split("\n"):
+                # Get gene info
+                info = line.decode("utf-8").split("\t")
+                entrezid = info[1]
+                genename = info[2]
+                synonyms = info[4].split("|")
 
-        # i.e. a mapping
-        m = re.search("(\w+)\t(\w+)", line)
-        if m:
+                # If valid entrezid...
+                if info[1] not in entrezids:
+                    continue
 
-            # Map uniacc to genename
-            uniacc, genename = line.split("\t")
-            uniacc2genename.setdefault(uniacc, genename)
+                # Map entrezid to gene names
+                entrezid2genename.setdefault(entrezid, [])
+                entrezid2genename[entrezid].append(genename)
+                for synonym in synonyms:
+                    entrezid2genename[entrezid].append(synonym)
 
-    #     # Write pickle file
-    #     with open(pickle_file, "wb") as f:
-    #         pickle.dump(uniacc2genename, f)
+        if os.path.exists(file_name):
+            os.remove(file_name)
 
-    # # Load pickle file
-    # with open(pickle_file, "rb") as f:
-    #     uniacc2genename = pickle.load(f)
+        # Write pickle file
+        with open(pickle_file, "wb") as f:
+            pickle.dump(entrezid2genename, f)
 
-    return(uniacc2genename)
+    # Load pickle file
+    with open(pickle_file, "rb") as f:
+        entrezid2genename = pickle.load(f)
 
-def _make_word_cloud(uniacc):
+    return(entrezid2genename)
+
+def _make_word_cloud(uniacc, max_words=50):
 
     # Skip if already created
     png_file = "%s.png" % uniacc
     if not os.path.exists(png_file):
 
         # Get pmid
-        cmd = "Rscript ../make_word_cloud.R %s" % uniacc
+        cmd = "Rscript ../make_word_cloud.R %s %s" % (uniacc, max_words)
         process = subprocess.run([cmd], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 #-------------#
--- tf-idfs/make_word_cloud.R
+++ tf-idfs/make_word_cloud.R
@@ -5,6 +5,7 @@ library(PubMedWordcloud)
 
 args = commandArgs(trailingOnly=TRUE)
 uniacc = args[1]
+max_words = as.integer(args[2])
 
 plot_word_cloud<-function(uniacc){
     rdsfile=paste(uniacc,".rds", sep="")
@@ -14,7 +15,9 @@ plot_word_cloud<-function(uniacc){
     tfidfs=data.frame(rdsdata$Var1, rdsdata$tfidf)
     colnames(tfidfs)<-c("word", "freq")
     tfidfs<-tfidfs[order(tfidfs$freq, decreasing=TRUE),]
-    plotWordCloud(tfidfs[1:nrow(tfidfs),], min.freq=0, scale=c(3, 1), max.words=50)
+    iranks <- max_words - c(1:nrow(tfidfs))
+    tfidfs$freq<-iranks
+    plotWordCloud(tfidfs[1:nrow(tfidfs),], min.freq=0, scale=c(3, 1), max.words=max_words)
     dev.off()
 }
 
